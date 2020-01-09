ac2bcb9ad2783fcff58b5f19209eda2e7ba43d25
Leon Malisov <leonmalisov@gmail.com>
2020-01-09 23:24:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,13 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{katz_deli.each_with_index {|customer, index| print "#{index + 1}. #{customer} "}}"
+    puts "The line is currently: #{katz_deli.each_with_index { |customer, index| print "#{index + 1}. #{customer} "}}"
   end
+end
+
+puts "The line is currently: #{katz_deli.each_with_index { |customer, index| print "#{index + 1}. #{customer} "}}"
+
+deli_line = ["Logan", "Avi", "Spencer"]
+deli_line.each do |customer|
+  puts deli_line.index(customer)
 end
