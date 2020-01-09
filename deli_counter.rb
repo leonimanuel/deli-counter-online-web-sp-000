177ffe2d3f29008fd93732161919dbc93b1d6d17
Leon Malisov <leonmalisov@gmail.com>
2020-01-09 23:16:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(katz_deli)
   if katz_deli.size == 0
-    puts "the line is currently empty"
+    puts "the line is currently empty."
   else
     puts "The line is currently: #{katz_deli.each_with_index {|customer, index| print "#{index + 1}. #{customer} "}}"
   end
