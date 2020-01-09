89a8e7c21da8165fddd89e20a7d212ad92e7610a
Leon Malisov <leonmalisov@gmail.com>
2020-01-09 23:59:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def take_a_number(katz_deli, last_person)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.size = 0
+  if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
   else
     puts "Now serving #{katz_deli[0]}!"
