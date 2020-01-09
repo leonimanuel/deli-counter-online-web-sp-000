ad2698687ff2ea234783e64256d47439c60412a5
Leon Malisov <leonmalisov@gmail.com>
2020-01-09 23:38:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,22 @@
 # Write your code here.
 
 def line(katz_deli)
+  katz_deli.each do |customer|
+    "#{katz_deli.index(customer) + 1}. #{customer} "
+  end
+
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
     puts katz_deli.each do |customer|
       "#{katz_deli.index(customer) + 1}. #{customer} "
     end
-    print katz_deli.join
   end
 end
 
 # puts "The line is currently: #{katz_deli.each_with_index { |customer, index| print "#{index + 1}. #{customer} "}}"
 
-# deli_line = ["Logan", "Avi", "Spencer"]
-# deli_line.each do |customer|
-#   print "#{deli_line.index(customer) + 1}. #{customer} "
-# end
+deli_line = ["Logan", "Avi", "Spencer"]
+deli_line.each do |customer|
+  "#{deli_line.index(customer) + 1}. #{customer} "
+end
