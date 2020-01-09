a9ff1244bb999f3c814078d4bede8fd9bda54fa2
Leon Malisov <leonmalisov@gmail.com>
2020-01-09 23:25:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,5 +11,5 @@ puts "The line is currently: #{katz_deli.each_with_index { |customer, index| pri
 
 deli_line = ["Logan", "Avi", "Spencer"]
 deli_line.each do |customer|
-  puts deli_line.index(customer)
+  print deli_line.index(customer) + 1
 end
