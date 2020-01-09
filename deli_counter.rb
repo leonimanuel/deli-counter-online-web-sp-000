2b1d120f3d511677e305188385da66bf156d5a1e
Leon Malisov <leonmalisov@gmail.com>
2020-01-09 23:43:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{deli_count.join}"
+    puts "The line is currently: #{deli_count.join.chomp}"
   end
 end
 
