8491305342a25c90f57ff6db09e6bb4ec95577b4
Leon Malisov <leonmalisov@gmail.com>
2020-01-09 23:58:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,11 @@ def take_a_number(katz_deli, last_person)
 end
 
 def now_serving(katz_deli)
+  if katz_deli.size = 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Now serving #{katz_deli[0]}!"
+    katz_deli.shift
 end
 # puts "The line is currently: #{katz_deli.each_with_index { |customer, index| print "#{index + 1}. #{customer} "}}"
 
